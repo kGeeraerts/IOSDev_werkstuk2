463ecbd49c25fc8124b033a0df1ed6c18cfd51de
kGeeraerts <kwinten.geeraerts@student.ehb.be>
2021-07-31 07:30:05 -0700

    Initial Commit

---
+++ IOSDev_werkstuk2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSDev_werkstuk2
+//
+//  Created by Quceka on 31/07/2021.
+//  Copyright © 2021 Quceka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
